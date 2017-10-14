58901c914571c1d20d8fe11968620bd5fd343747
cawatou <cawatou@gmail.com>
2017-10-14 17:38:55 +0300

    move server

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,13 +1,7 @@
 <?php
 
-/* @var $this \yii\web\View */
-/* @var $content string */
-
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
 use yii\bootstrap\ActiveForm;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 use app\models\ContactForm;
 AppAsset::register($this);
